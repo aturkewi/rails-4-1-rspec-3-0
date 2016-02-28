b0637712b1cd798660825ab4f4eba12471d49947
Avidor Turkewitz <aviturkewitz@gmail.com>
2016-02-28 08:59:41 -0500

    Add tests for get requests

--- spec/controllers/contacts_controller_spec.rb
+++ spec/controllers/contacts_controller_spec.rb
@@ -4,29 +4,66 @@ describe ContactsController do
 
   describe 'GET #index' do
     context 'with params[:letter]' do
-      it 'populates an array of contacts with this the letter'
-      it 'renders the :index template'
+      it 'populates an array of contacts with this the letter' do
+        smith = create(:contact, lastname: 'Smith')
+        jones = create(:contact, lastname: 'Jones')
+        get :index, letter:'S'
+        expect(assigns(:contacts)).to match_array([smith])
+      end
+      it 'renders the :index template' do
+        get :index, letter: "S"
+        expect(response).to render_template :index
+      end
     end
 
     context 'without params[:letter]' do
-      it 'populates an array of all contacts'
+      it 'populates an array of all contacts' do
+        Contact.destroy_all
+        smith = create(:contact, lastname: 'Smith')
+        jones = create(:contact, lastname: 'Jones')
+        drew = create(:contact, lastname: 'Drew')
+        get :index
+        expect(assigns(:contacts)).to match_array([smith, jones, drew])
+      end
       it 'renders the :index template'
     end
   end
 
   describe 'GET #show' do
-    it 'assigns the requested contacts to @contact'
-    it 'renders the :show template'
+    it 'assigns the requested contacts to @contact' do
+      contact = create(:contact)
+      get :show, id: contact
+      expect(assigns(:contact)).to eq contact
+    end
+    it 'renders the :show template' do
+      contact = create(:contact)
+      get :show, id: contact
+      expect(response).to render_template :show
+    end
   end
 
   describe 'GET #new' do
-    it 'assigns a new Contact to @contact'
-    it 'renders the :new template'
+    it 'assigns a new Contact to @contact' do
+      get :new
+      expect(assigns(:contact)).to be_a_new(Contact)
+    end
+    it 'renders the :new template' do
+      get :new
+      expect(response).to  render_template :new
+    end
   end
 
   describe 'GET #edit' do
-    it 'assigns the requested contact to @contact'
-    it 'redners the :edit template'
+    it 'assigns the requested contact to @contact' do
+      contact = create(:contact)
+      get :edit, id: contact
+      expect(assigns(:contact)).to eq contact
+    end
+    it 'redners the :edit template' do
+      contact = create(:contact)
+      get :edit, id: contact
+      expect(response).to render_template :edit
+    end
   end
 
   describe 'POST #create' do
@@ -34,7 +71,7 @@ describe ContactsController do
       it 'saves the new contact in the database'
       it 'redirects to contacs#show'
     end
-    
+
     context 'with invalid attributes' do
       it 'does not save the new contact in the database'
       it 're-renders the :new template'
--- spec/factories/contacts.rb
+++ spec/factories/contacts.rb
@@ -10,5 +10,9 @@ FactoryGirl.define do
       end
     end
 
+    factory :invalid_contact do
+      firstname nil
+    end
+
   end
 end
